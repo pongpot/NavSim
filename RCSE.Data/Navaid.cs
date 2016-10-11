dee1755e9ca497ec9e0d5b3543af488483e6df23
pongpot <pongpot.c@gmail.com>
2016-10-11 15:37:47 +0700

    Add Navaid data object

---
+++ RCSE.Data/Navaid.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RCSE.Data
+{
+    public class Navaid
+    {
+        public enum StatusType
+        {
+            Off,
+            Normaml,
+            Warning,
+            Alarm,
+            Maintenance,
+            Maintenanceflash,
+            Datacom,
+            Datacomflash
+        };
+
+        public string Name { get; set; }
+        public StatusType Status { get; set; }
+        public bool Control { get; set; }
+        public int Runway { get; set; }
+        public int Rpvnum { get; set; }
+        public Navaid() { }
+        public Navaid(string name, StatusType status, bool control, int runway, int rpvnum)
+        {
+            Name = name;
+            Status = status;
+            Control = control;
+            Runway = runway;
+            Rpvnum = rpvnum;
+        }
+        //Other properties, methods, events...
+    }
+}
